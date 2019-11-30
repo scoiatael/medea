2191bd92e40bfe3823626942138b6750e32615f2
lukasz.czaplinski <lukasz.czaplinski@tooploox.com>
2019-11-30 16:21:04 +0100

    Chore: run rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,21 +12,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_141938) do
-
+ActiveRecord::Schema.define(version: 20_191_130_141_938) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pgcrypto"
-  enable_extension "plpgsql"
-  enable_extension "postgis"
-  enable_extension "uuid-ossp"
+  enable_extension 'pgcrypto'
+  enable_extension 'plpgsql'
+  enable_extension 'postgis'
+  enable_extension 'uuid-ossp'
 
-  create_table "locations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.text "name", null: false
-    t.geometry "lonlat", limit: {:srid=>0, :type=>"st_point"}
-    t.boolean "inside"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "geocoder_errors", default: [], array: true
+  create_table 'locations', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.text 'name', null: false
+    t.geometry 'lonlat', limit: { srid: 0, type: 'st_point' }
+    t.boolean 'inside'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'geocoder_errors', default: [], array: true
   end
-
 end
